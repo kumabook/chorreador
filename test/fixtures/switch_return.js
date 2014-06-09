58a92badd867632bb6c70c518b3a20bad4acf27b
kumabook <kumabook@live.jp>
2014-06-09 10:36:54 +0900

    Add tests

---
+++ test/fixtures/switch_return.js
@@ -0,0 +1,9 @@
+var test = function(i) {
+  switch (i) {
+    case 0: return 0;
+    case 1: {
+      return 1;
+    }
+    default: return -1;
+  }
+};
---
+++ test/fixtures/tracer.js
@@ -0,0 +1,2 @@
+window.logger = function() {
+};
