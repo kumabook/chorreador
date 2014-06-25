feef5262ac3339c0a6bb7f65d4642b89fe1c3147
kumabook <kumabook@live.jp>
2014-06-25 11:03:09 +0900

    Add sample js files for instrumentor test

---
+++ test/fixtures/if_return.js
@@ -0,0 +1,8 @@
+var test = function (i) {
+  if (i == 0)
+    return 0;
+  else if (i == 1)
+    return 1;
+  else
+    return 2;
+};
---
+++ test/fixtures/while_return.js
@@ -0,0 +1,4 @@
+var test = function() {
+  while (true)
+    return;
+};
